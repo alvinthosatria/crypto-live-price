aea6d7d81c80e459dcfd29036f7d11ffa7a854c5
alvinthosatria <alvinthosatria@gmail.com>
2024-02-02 22:42:19 +0800

    fix: lint & default port value

--- crypto-realtime-price/api/index.js
+++ crypto-realtime-price/api/index.js
@@ -1,24 +1,23 @@
-import express from 'express';
-import http from 'http';
-import { WebSocketServer } from 'ws';
-import * as dotenv from 'dotenv';
-import fetch from 'node-fetch';
-import { createClient } from 'redis';
+import express from "express";
+import http from "http";
+import { WebSocketServer } from "ws";
+import * as dotenv from "dotenv";
+import fetch from "node-fetch";
+import { createClient } from "redis";
 
 dotenv.config();
 
 const app = express();
 const apiKey = process.env.API_KEY;
-const PORT1 = process.env.PORT_ONE;
-const PORT2 = process.env.PORT_TWO;
+const PORT1 = process.env.PORT_ONE ?? 8800;
+const PORT2 = process.env.PORT_TWO ?? 8801;
 const DEFAULT_EXPIRATION = 19;
-
 
 //middleware
 app.use(express.json());
-const redisClient  = createClient();
+const redisClient = createClient();
 
-(async() => {
+(async () => {
   await redisClient.connect();
 })();
 
@@ -33,21 +32,21 @@ const wss = new WebSocketServer({
 
   //handshake connection with NGINX proxy server
   verifyClient: (info, cb) => {
-    const forwardedFor = info.req.headers['x-forwarded-for'];
+    const forwardedFor = info.req.headers["x-forwarded-for"];
     if (forwardedFor) {
-      info.req.remoteAddress = forwardedFor.split(',')[0];
+      info.req.remoteAddress = forwardedFor.split(",")[0];
     }
     cb(true);
-  }
+  },
 });
 
 const requestOptions = {
-  method: 'GET',
+  method: "GET",
   headers: {
-    'Content-Type': 'application/json',
-    'Authorization': `Bearer ${apiKey}`,
-    'Accept-Encoding': 'gzip',
-  }
+    "Content-Type": "application/json",
+    Authorization: `Bearer ${apiKey}`,
+    "Accept-Encoding": "gzip",
+  },
 };
 
 let priceUsd;
@@ -58,20 +57,24 @@ const convertItem = (item) => {
   priceUsd = parseFloat(item.priceUsd).toFixed(8).toString();
   volumeUsd = parseFloat(item.volumeUsd24Hr).toFixed(8).toString();
   changePercent = parseFloat(item.changePercent24Hr).toFixed(8).toString();
-}
+};
 
 const fetchCryptoData = async () => {
-  const res = await fetch('https://api.coincap.io/v2/assets?limit=10', requestOptions);
-  if (res.status !== 200) throw new Error(`Failed to fetch data. Status code: ${res.status}`);
+  const res = await fetch(
+    "https://api.coincap.io/v2/assets?limit=10",
+    requestOptions
+  );
+  if (res.status !== 200)
+    throw new Error(`Failed to fetch data. Status code: ${res.status}`);
   const data = await res.json();
   const info = data.data.map((item) => {
     convertItem(item);
-    return { 
-        id: item.id,
-        name: item.name,
-        price: priceUsd,
-        volume: volumeUsd,
-        change: changePercent,
+    return {
+      id: item.id,
+      name: item.name,
+      price: priceUsd,
+      volume: volumeUsd,
+      change: changePercent,
     };
   });
   return info;
@@ -90,11 +93,14 @@ const request = async (ws) => {
 
     const info = await fetchCryptoData();
     apiHits++;
-    console.log(apiHits);
 
     // Create a new cache object with a 19 seconds expiration time because
     // data from CoinCap API is updated every 20 seconds
-    await redisClient.setEx('crypto_data', DEFAULT_EXPIRATION, JSON.stringify(info));
+    await redisClient.setEx(
+      "crypto_data",
+      DEFAULT_EXPIRATION,
+      JSON.stringify(info)
+    );
 
     ws.send(JSON.stringify(info));
   } catch (error) {
@@ -102,18 +108,17 @@ const request = async (ws) => {
   }
 };
 
-wss.on('connection', (ws) => {
-    ws.on('error', console.error);
-    console.log('Client connected!');
-    request(ws);
-    setInterval(() => request(ws), 20000);
-    ws.on('disconnect', () => {
-        console.log('Client disconnected!');
-        redisClient.quit();
-    });
+wss.on("connection", (ws) => {
+  ws.on("error", console.error);
+  console.log("Client connected!");
+  request(ws);
+  setInterval(() => request(ws), 20000);
+  ws.on("disconnect", () => {
+    console.log("Client disconnected!");
+    redisClient.quit();
+  });
 });
 
-
 httpServer.listen(PORT1 || PORT2, () => {
-  console.log('Server is running!')
+  console.log("Server is running!");
 });
